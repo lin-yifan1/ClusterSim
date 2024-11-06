d43bcdd0c964a8d1e0e6cab2296357231b9d0498
nick <yifanlin@mail.ustc.edu.cn>
2024-11-06 10:43:01 +0800

    Modify `cal_time_shifts`

--- solver/time_shifts.py
+++ solver/time_shifts.py
@@ -7,13 +7,17 @@ def cal_time_shifts(traffic_manager: TrafficManager):
     time_shifts = {}
     for link, jobs in traffic_manager.link_traffic_pattern.items():
         time_shifts[link] = {}
+
+        link_job_num = len(jobs)  # number of jobs on the link
+        T_min = min([pattern["T"] for pattern in jobs.values()])  # Min T of the link
+        interval_len = T_min // link_job_num  # Interval length between job traffics
+
         jobs_sorted = list(
-            sorted(jobs.items(), key=lambda item: item[1]["T"], reverse=True)
-        )
-        first_job_name, first_pattern = jobs_sorted[0]
-        first_start_time = traffic_manager.job_time_period[first_job_name][0]
-        first_interval_start = first_pattern["intervals"][0][0]
-        start_point = first_start_time + first_interval_start
+            sorted(jobs.items(), key=lambda item: item[1]["T"])
+        )  # Deploy jobs with small Ts first
+
+        start_point = 0
+
         for job_name, pattern in jobs_sorted:
             start_time = traffic_manager.job_time_period[job_name][0]
             interval_start = pattern["intervals"][0][0]
@@ -21,7 +25,6 @@ def cal_time_shifts(traffic_manager: TrafficManager):
             time_shifts[link][job_name] = (
                 start_point - (start_time + interval_start)
             ) % T
-            interval_len = pattern["intervals"][0][1] - pattern["intervals"][0][0]
             start_point += interval_len
     return time_shifts
 
