54e271a82789e7acdb8b437af0e47542caaba5dd
nick <yifanlin@mail.ustc.edu.cn>
2024-11-08 17:54:28 +0800

    Modify `cal_time_shifts_cassini`

--- solver/time_shifts.py
+++ solver/time_shifts.py
@@ -35,22 +35,15 @@ def cal_time_shifts_cassini(traffic_manager: TrafficManager):
     time_shifts = {}
     for link, jobs in traffic_manager.link_traffic_pattern.items():
         time_shifts[link] = {}
-        jobs_list = list(jobs.items())
-        first_job_name, first_pattern = jobs_list[0]
-        first_start_time = traffic_manager.job_time_period[first_job_name][0]
-        first_interval_start = first_pattern["intervals"][0][0]
-        start_point = (
-            first_start_time + first_interval_start
-        )  # time spot which next job should be placed
-        for job_name, pattern in jobs_list:
+        start_point = 0
+        for job_name, pattern in jobs.items():
             start_time = traffic_manager.job_time_period[job_name][0]
-            interval_start, interval_end = pattern["intervals"][0]
+            interval_start = pattern["intervals"][0][0]
+            interval_end = pattern["intervals"][0][1]
+            interval_len = interval_start - interval_end
             T = pattern["T"]
             time_shifts[link][job_name] = (
                 start_point - (start_time + interval_start)
             ) % T
-            interval_len = interval_end - interval_start
             start_point += interval_len
-        for job_name in jobs.keys():
-            time_shifts[link][job_name] = 0
     return time_shifts
